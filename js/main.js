a5b01a890d19a168aa24e7f7c62fa3006c34db11
owlstechbd <owlstech.net@gmail.com>
2024-01-16 22:00:47 +0600

    price section add

--- js/main.js
+++ js/main.js
@@ -39,9 +39,59 @@ $(function(){
     const videoBackgrounds = VIDEO_BACKGROUNDS;
   6
   });
-  7
-   
+  
+//hover effect
+  document.getElementById('box2').addEventListener('mouseover', function() {
+    document.getElementById('box1').classList.add('hovered');
+  });
+  
+  document.getElementById('box2').addEventListener('mouseout', function() {
+    document.getElementById('box1').classList.remove('hovered');
+  });
+  
+  document.getElementById('box3').addEventListener('mouseover', function() {
+    document.getElementById('box1').classList.add('hovered');
+  });
+  
+  document.getElementById('box3').addEventListener('mouseout', function() {
+    document.getElementById('box1').classList.remove('hovered');
+  });
+  
+  document.getElementById('box4').addEventListener('mouseover', function() {
+    document.getElementById('box1').classList.add('hovered');
+  });
+  
+  document.getElementById('box4').addEventListener('mouseout', function() {
+    document.getElementById('box1').classList.remove('hovered');
+  });
+  
+//typing effect
+const typingText = document.getElementById("typing-text");
+const targetSection = document.getElementById("target-section");
 
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    typeText(); // Start typing effect when section enters viewport
+  } else {
+    typingText.textContent = ""; // Clear text when section leaves viewport
+  }
+});
+
+observer.observe(targetSection);
+
+const textToType = "Alexa! I know your work day’s been crazy, so I shortned your workout. Consistency is our motto! Let’s keep that treak going.";
+let index = 0;
+
+function typeText() {
+  if (index < textToType.length) {
+    typingText.textContent += textToType.charAt(index);
+    index++;
+    setTimeout(typeText, Math.random() * 5 + 50);
+  } else {
+    // Display full text without animation after typing effect finishes
+    typingText.textContent = textToType;
+  }
+}
 
 
 
